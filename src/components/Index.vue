61cebd94ba92a1db4c19dcdb92927e754cf2a718
bvanh <48560384+bvanh@users.noreply.github.com>
2020-11-11 14:21:24 +0700

    f

---
+++ src/components/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="app-content">
+    <v-row class="text-center">
+      <v-col>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to WARA</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col md="4">
+        <Menu />
+      </v-col>
+      <v-col md="20">
+        <TopNap />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Menu from "./Menu";
+import TopNap from "./TopNap";
+export default {
+  name: "Index",
+  data: () => ({
+    demo: "",
+  }),
+  components: {
+    Menu,
+    TopNap,
+  },
+};
+</script>
+<style src='./style.scss' lang='scss' scoped>
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="mx-auto" max-width="100%" tile>
+    <v-list rounded>
+      <v-subheader class='font-weight-bold'>REPORTS</v-subheader>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectedItem: 0,
+    items: [
+      { text: "Top Nạp", icon: "mdi-alpha-c-circle-outline" },
+      { text: "Top Level", icon: "mdi-alpha-l-circle-outline" },
+      { text: "Real-Time", icon: "mdi-clock" },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
